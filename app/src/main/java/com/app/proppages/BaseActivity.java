f3e580bd7b945ebf436e2ea9c5bf30f0a2f57610
encreddesign <encredgaming@gmail.com>
2017-03-29 21:02:13 +0100

    Added needed thread handlers and tasks

--- app/src/main/java/com/app/proppages/BaseActivity.java
+++ app/src/main/java/com/app/proppages/BaseActivity.java
@@ -2,18 +2,38 @@ package com.app.proppages;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
+import com.app.proppages.callbacks.HttpBackgroundTask;
+import com.app.proppages.callbacks.WorkerThread;
 import com.app.proppages.utils.UtilBase;
 import com.app.proppages.view.BaseFragment;
 import com.app.proppages.view.FragmentMap;
 
 public class BaseActivity extends Activity {
 
+    // important handler for UI
+    // VERY important, remove this and everything crumbles :(
+    protected Handler mUiHandler;
+    protected WorkerThread mWorkerThread;
+
+    // our background task for http operations
+    private HttpBackgroundTask mHttpBackgroundTask;
+
+    private static final String wThreadLabel = "OurWorkingThread";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        // register the UI handler
+        this.mUiHandler = new Handler();
+        this.mWorkerThread = new WorkerThread(wThreadLabel);
+
+        // create a new instance of our background thread and pass our UI handler
+        this.mHttpBackgroundTask = HttpBackgroundTask.newInstance( this.getApplicationContext(), this.mUiHandler );
 
         if( findViewById(R.id.base_frame) != null ) {
 
@@ -27,6 +47,12 @@ public class BaseActivity extends Activity {
             }
 
         }
+
+        // start our working thread
+        this.mWorkerThread.start();
+        // prepare our handler
+        this.mWorkerThread.prepHandler();
+        this.mWorkerThread.postTask(this.mHttpBackgroundTask);
 
     }
 
@@ -54,4 +80,11 @@ public class BaseActivity extends Activity {
         super.onResume();
     }
 
+    @Override
+    protected void onDestroy() {
+
+        this.mWorkerThread.quit();
+        super.onDestroy();
+
+    }
 }
