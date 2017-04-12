276eda5726deb5d4e250bd3d2b8a50027f7a9346
encreddesign <encredgaming@gmail.com>
2017-04-12 19:09:25 +0100

    Added list view item click listener

---
+++ app/src/main/java/com/app/proppages/callbacks/OnListItemSelect.java
@@ -0,0 +1,18 @@
+package com.app.proppages.callbacks;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by Joshua on 12/04/17.
+ */
+public class OnListItemSelect implements AdapterView.OnItemClickListener {
+
+    public OnListItemSelect () {}
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+}
