4b939eff93a6a2a3650d6da5f324aaa7f78460ee
encreddesign <encredgaming@gmail.com>
2017-04-19 20:31:49 +0100

    added callbacks for refreshing list

--- app/src/main/java/com/app/proppages/tasks/SwipeBackgroundTask.java
+++ app/src/main/java/com/app/proppages/tasks/SwipeBackgroundTask.java
@@ -7,7 +7,15 @@ import com.app.proppages.BaseActivity;
 import com.app.proppages.callbacks.ImageCacheCallback;
 import com.app.proppages.enums.ContentTypes;
 import com.app.proppages.enums.Routes;
+import com.app.proppages.http.OnDoneImageCache;
 import com.app.proppages.utils.UtilBase;
+import com.app.proppages.utils.UtilJson;
+import com.app.proppages.view.ProfilesFragment;
+import com.app.proppages.view.model.ProfileModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Joshua on 17/04/17.
@@ -33,7 +41,7 @@ public class SwipeBackgroundTask extends HttpTaskAbstract implements Runnable {
             if(response != null) {
 
                 // load profile images in mem cache first, so we can grab later
-                //BaseActivity.getICache().loadImageInCache( response, this, ImageCacheCallback.newInstance(this.activity, this.hUiHandler) );
+                BaseActivity.getICache().loadImageInCache( response, this, new RefreshImageCallback() );
 
             } else {
                 throw new Exception("Swipe http response null");
@@ -41,6 +49,54 @@ public class SwipeBackgroundTask extends HttpTaskAbstract implements Runnable {
 
         } catch (Exception ex) {
             Log.e( UtilBase.LOG_TAG, "Swipe Runnable", ex );
+        }
+
+    }
+
+    /*
+    * @class RefreshImageCallback
+    * */
+    private class RefreshImageCallback implements OnDoneImageCache {
+
+        @Override
+        public void complete(String data) {
+
+            final List<ProfileModel> arrayData = new ArrayList<ProfileModel>();
+            if( data != null ) {
+
+                final ArrayList<HashMap<String, String>> profileArrData = UtilJson.getAsArray(data);
+                for(HashMap<String, String> arr : profileArrData) {
+
+                    arrayData.add(ProfileModel.newInstance(arr));
+
+                }
+
+                mUiHandler.post( new RefreshList(arrayData) );
+
+            } else {
+                Log.e( UtilBase.LOG_TAG, "SwipeBackgroundTask problem...no http response" );
+            }
+
+        }
+
+        @Override
+        public void processed(int number) {}
+    }
+
+    /*
+    * @class RefreshList
+    * */
+    private class RefreshList implements Runnable {
+
+        List<ProfileModel> models;
+
+        public RefreshList ( List<ProfileModel> models ) {
+            this.models = models;
+        }
+
+        @Override
+        public void run() {
+            ProfilesFragment.updateList( this.models );
         }
 
     }
